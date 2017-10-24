46218d78c9a876a4c059cbe00a8cc89815aa6987
lost273 <dnk273@gmail.com>
2017-10-24 15:40:32 +0300

    24-10-2017-15-40

---
+++ WebServices/Controllers/ReservationController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using WebServices.Models;
+
+namespace WebServices.Controllers {
+    public class ReservationController : ApiController {
+        private IReservationRepository repo = ReservationRepository.getRepository();
+        public IEnumerable<Reservation> GetAllReservations () {
+            return repo.GetAll();
+        }
+        public Reservation GetReservation (int id) {
+            return repo.Get(id);
+        }
+        [HttpPost]
+        public Reservation CreateReservation (Reservation item) {
+            return repo.Add(item);
+        }
+        [HttpPut]
+        public bool UpdateReservation (Reservation item) {
+            return repo.Update(item);
+        }
+        public void DeleteReservation (int id) {
+            repo.Remove(id);
+        }
+    }
+}
